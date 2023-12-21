4d8bc558462d74644cd32272c13644aec3ab7a36
EXB04891 <agustina.aliciardi@darwoft.com>
2023-12-21 09:58:43 -0300

    Config report

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
